5b5346796fc41669e4bb10c1b766efead8f2abcd
joaoclemos <joaoclemos22@gmail.com>
2025-07-01 18:56:27 -0300

    Q1 att

--- Q1.c
+++ Q1.c
@@ -4,38 +4,32 @@
 #define NUM_PESSOAS 40
 #define MAX_NOME 50
 
-// Definindo a struct Pessoa
-struct Pessoa {
+typedef struct pessoa {
     char nome[MAX_NOME];
     int dia;
     int mes;
-};
+} pessoa;
 
 int main() {
-    struct Pessoa pessoas[NUM_PESSOAS];
-    int i, mes;
-
-    // Lendo os dados das 40 pessoas
-    for (i = 0; i < NUM_PESSOAS; i++) {
-        printf("Digite o nome da pessoa %d: ", i + 1);
-        fgets(pessoas[i].nome, MAX_NOME, stdin);
-        // Remover o caractere de nova linha, se presente
-        pessoas[i].nome[strcspn(pessoas[i].nome, "\n")] = 0;
-
-        printf("Digite o dia do aniversário: ");
-        scanf("%d", &pessoas[i].dia);
-        printf("Digite o mês do aniversário: ");
-        scanf("%d", &pessoas[i].mes);
-        // Limpar o buffer de entrada
+    pessoa lista[NUM_PESSOAS];
+    
+    // Leitura dos dados
+    for (int i = 0; i < NUM_PESSOAS; i++) {
+        printf("Insira os dados (%d):\n", i + 1);
+        printf("Nome: ");
+        scanf(" %49[^\n]", lista[i].nome);
+        printf("Dia: ");
+        scanf("%d", &lista[i].dia);
+        printf("Mês: ");
+        scanf("%d", &lista[i].mes);
         while (getchar() != '\n');
     }
-
-    // Exibindo os aniversariantes de cada mês
-    for (mes = 1; mes <= 12; mes++) {
+    
+    for (int mes = 1; mes <= 12; mes++) {
         printf("\nAniversariantes do mes %d:\n", mes);
-        for (i = 0; i < NUM_PESSOAS; i++) {
-            if (pessoas[i].mes == mes) {
-                printf("Nome: %s, Dia: %d\n", pessoas[i].nome, pessoas[i].dia);
+        for (int i = 0; i < NUM_PESSOAS; i++) {
+            if (lista[i].mes == mes) {
+                printf("Nome: %s, Dia: %d\n", lista[i].nome, lista[i].dia);
             }
         }
     }
